0191a4c3079d4d14837af3ae4aaa64f7e646e537
Woofer <61364477+Woofer21@users.noreply.github.com>
2025-05-04 15:40:25 -0400

    chore: add link to get to game conf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
 	return (
 		<main className="flex flex-col items-center justify-center gap-4 p-4">
 			<h1 className="text-4xl font-bold">Dart Score Keeper</h1>
-			<Button>Start Game</Button>
+			<Link href={"/start"}>
+				<Button>Start Game</Button>
+			</Link>
 		</main>
 	);
 }
